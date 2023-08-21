13d1208e954b125ed09d1a0aba37b8085de6bb5f
Simeon Iksanov <Simeon.Iksanov@gmail.com>
2023-08-21 21:03:15 +0500

    fix dto

--- src/SportBids.Application/Authentication/Common/AuthResult.cs
+++ src/SportBids.Application/Authentication/Common/AuthResult.cs
@@ -5,6 +5,7 @@ namespace SportBids.Application.Authentication.Common;
 public class AuthResult
 {
     public string UserName { get; set; } = null!;
+    public string Email { get; set; } = null!;
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
 
